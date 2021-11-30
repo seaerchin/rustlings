46f23333a2a6a31b7171b4af36630d3322ed4cfd
seaerchin <jer005@e.ntu.edu.sg>
2021-12-01 03:21:07 +0800

    feat(traits): add code

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -8,14 +8,16 @@
 // which appends "Bar" to any object
 // implementing this trait.
 
-// I AM NOT DONE
+use std::ops::Add;
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 impl AppendBar for String {
-    //Add your code here
+    fn append_bar(self) -> Self {
+        self.add("Bar")
+    }
 }
 
 fn main() {
