e826c544a146cd3d150e90b4063b51af225965a5
seaerchin <jer005@e.ntu.edu.sg>
2021-11-30 17:09:55 +0800

    feat(variables): add chapter solutions

--- exercises/variables/variables3.rs
+++ exercises/variables/variables3.rs
@@ -1,11 +1,11 @@
 // variables3.rs
 // Make me compile! Execute the command `rustlings hint variables3` if you want a hint :)
 
-// I AM NOT DONE
-
 fn main() {
     let x = 3;
     println!("Number {}", x);
-    x = 5; // don't change this line
+    // shadowing occurs here
+    // we are reusing the same label but variable data is different.
+    let x = 5; // don't change this line
     println!("Number {}", x);
 }
