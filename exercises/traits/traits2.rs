3597d50e350d2253aa8437ab204a782c642cd9f7
seaerchin <jer005@e.ntu.edu.sg>
2021-12-01 03:34:59 +0800

    feat(traits): add extra impl

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -9,6 +9,7 @@
 //
 // No boiler plate code this time,
 // you can do this!
+use std::ops::Add;
 
 trait AppendBar {
     fn append_bar(self) -> Self;
@@ -17,6 +18,19 @@ trait AppendBar {
 impl AppendBar for Vec<String> {
     fn append_bar(mut self) -> Self {
         self.push(String::from("Bar"));
+        self
+    }
+}
+
+trait AppendEachBar {
+    fn append_each(self) -> Self;
+}
+
+impl AppendEachBar for Vec<String> {
+    fn append_each(mut self) -> Self {
+        for s in self.iter_mut() {
+            s.push_str("Bar");
+        }
         self
     }
 }
@@ -30,5 +44,11 @@ mod tests {
         let mut foo = vec![String::from("Foo")].append_bar();
         assert_eq!(foo.pop().unwrap(), String::from("Bar"));
         assert_eq!(foo.pop().unwrap(), String::from("Foo"));
+    }
+
+    #[test]
+    fn test_append_each() {
+        let mut foo = vec![String::from("Foo")].append_each();
+        assert_eq!(foo.pop().unwrap(), String::from("FooBar"));
     }
 }
